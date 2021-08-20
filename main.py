0fb04c94f2378a487af993e90db350eb20764e26
Gagas Surya Laksana <Gagassurya19@gmail.com>
2021-08-20 20:57:57 +0700

    change-time

--- main.py
+++ main.py
@@ -12,10 +12,7 @@ while True:
     WIB = pytz.timezone('Asia/Jakarta')
     time_now = datetime.now(WIB)
 
-    if (time_now.strftime('%H') == '05' and
-            time_now.strftime('%M') == '58' and
-            time_now.strftime('%a') != 'Sat' and
-            time_now.strftime('%a') != 'Sun'):
+    if (time_now.strftime('%H') == '05' and time_now.strftime('%M') == '58'):
         temp = scriptabsen.runscript(values.account(), values.sitelogger(), values.browser())
         times = datetime.now(WIB)
         if(temp == True):

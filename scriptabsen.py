7078e8c06d9c6c0538b98dd193cf9afa2549c161
Gagas Surya Laksana <Gagassurya19@gmail.com>
2021-08-20 12:13:34 +0700

    update main

--- scriptabsen.py
+++ scriptabsen.py
@@ -49,7 +49,8 @@ def runscript(account, sitelogger, browser):
     while True:
         WIB = pytz.timezone('Asia/Jakarta')
         time_now = datetime.now(WIB)
-        if time_now.strftime('%H') == '06' and time_now.strftime('%M') == '00':
+        # if time_now.strftime('%H') == '06' and time_now.strftime('%M') == '00':
+        if True:
             browser.refresh()
             if cek_absen(browser) == False:
                 absen(browser)
